f071433fe552679cbb01ef51ceadb794e3b0a794
Veer Saini <71731379+VeerTheDeer@users.noreply.github.com>
2026-07-13 16:01:17 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_sorted = "".join(sorted(s))
+        t_sorted = "".join(sorted(t))
+        
+        for i in range(len(s_sorted)):
+            if s_sorted[i] != t_sorted[i]:
+                return False
+                
+        return True
