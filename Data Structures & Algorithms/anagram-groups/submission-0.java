8fc37000eb48814f1b04f7dd8235baca96ff4b27
Veer Saini <71731379+VeerTheDeer@users.noreply.github.com>
2026-09-02 20:58:41 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++) {
+            char[] word = strs[i].toCharArray();
+            Arrays.sort(word);
+            String sortedString = new String(word);
+            if(map.containsKey(sortedString)){
+                map.get(sortedString).add(strs[i]);
+            } else {
+                List<String> newList = new ArrayList<>();
+                newList.add(strs[i]);
+                map.put(sortedString, newList);
+            }
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
